b01ec84c07434d9d0cb03c55dc79912e812ac430
Bernhard Fischer <bernhard.johannes.fischer@gmail.com>
2025-05-21 14:27:13 +0200

    Add main evasys class for the extension.

---
+++ classes/evasys.php
@@ -0,0 +1,35 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Class evasys.
+ * @package     bookingextension_evasys
+ * @copyright   2025 Wunderbyte GmbH <info@wunderbyte.at>
+ * @author      Bernhard Fischer-Sengseis
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace mod_booking\bookingextension;
+
+use mod_booking\plugininfo\bookingextension;
+use mod_booking\plugininfo\bookingextension_interface;
+
+/**
+ * Class for the Evasys booking extension.
+ */
+class evasys extends bookingextension implements bookingextension_interface {
+    // The plugin can implement functions defined in the interface here or extend the general plugin type.
+}
