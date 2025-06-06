b151bb400b58cbca75dbab8a36428afb1d868b12
Bernhard Fischer <bernhard.johannes.fischer@gmail.com>
2025-06-06 17:21:19 +0200

    Minor fixes (plugin not yet functional - just prepared so we can more easily migrate)

--- classes/evasys.php
+++ classes/evasys.php
@@ -60,13 +60,27 @@ class evasys extends bookingextension implements bookingextension_interface {
      */
     public function get_option_fields_info_array(): array {
         return [
-            'xtensiontestfield' => [
-                'name' => 'xtensiontestfield',
-                'class' => 'bookingextension_evasys\option\fields\xtensiontestfield',
+            'evasys' => [
+                'name' => 'evasys',
+                'class' => 'bookingextension_evasys\option\fields\evasys',
                 'id' => MOD_BOOKING_OPTION_FIELD_EVASYSTESTFIELD,
             ],
             // We can add more fields here...
         ];
+    }
+
+    /**
+     * Adds webservice functions to the service.
+     *
+     * This function usually includes settings.php file in plugins folder.
+     * Alternatively it can create a link to some settings page (instance of admin_externalpage)
+     *
+     * @param array $functions
+     * Reference to the array of functions to be registered with the web service.
+     */
+    public function register_booking_webservice_functions(array &$functions): void {
+        return; // No webservice functions to register for this plugin.
+        // If you want to register webservice functions, you can do it here.
     }
 
     /**
