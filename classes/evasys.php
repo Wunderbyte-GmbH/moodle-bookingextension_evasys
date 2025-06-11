6bec333156ea36bc04b6b6ba6d04635090c62db6
David Ala <davidala@Mac.dhw>
2025-06-11 14:28:35 +0200

    migrated settings and settings strings #1049

--- classes/evasys.php
+++ classes/evasys.php
@@ -25,9 +25,17 @@
 
 namespace bookingextension_evasys;
 
+use admin_setting_configcheckbox;
+use admin_setting_configpasswordunmask;
+use admin_setting_configselect;
+use admin_setting_configtext;
+use admin_setting_description;
+use admin_setting_heading;
 use admin_settingpage;
+use bookingextension_evasys\local\evasys_handler;
 use mod_booking\plugininfo\bookingextension;
 use mod_booking\plugininfo\bookingextension_interface;
+use SoapFault;
 
 defined('MOODLE_INTERNAL') || die();
 
@@ -101,7 +109,105 @@ class evasys extends bookingextension implements bookingextension_interface {
             'moodle/site:config',
             $this->is_enabled() === false
         );
+        $evasyssettings->add(
+            new admin_setting_heading(
+                'evasyssettings',
+                get_string('evasyssettings', 'mod_booking'),
+                get_string('evasyssettings_desc', 'mod_booking')
+            )
+        );
 
+        $evasyssettings->add(
+            new admin_setting_configcheckbox(
+                'booking/useevasys',
+                get_string('useevasys', 'mod_booking'),
+                get_string('useevasys_desc', 'mod_booking'),
+                0
+            )
+        );
+
+        $evasyssettings->add(
+            new admin_setting_configtext(
+                'booking/evasysbaseurl',
+                get_string('evasysbaseurl', 'mod_booking'),
+                get_string('evasysbaseurl_desc', 'mod_booking'),
+                ''
+            )
+        );
+
+        $evasyssettings->add(
+            new admin_setting_configtext(
+                'booking/evasysuser',
+                get_string('evasysuser', 'mod_booking'),
+                get_string('evasysuser_desc', 'mod_booking'),
+                ''
+            )
+        );
+        $evasyssettings->add(
+            new admin_setting_configpasswordunmask(
+                'booking/evasyspassword',
+                get_string('evasyspassword', 'mod_booking'),
+                get_string('evasyspassword_desc', 'mod_booking'),
+                ''
+            )
+        );
+        $userprofilefieldsarray[0] = get_string('userprofilefieldoff', 'mod_booking');
+        $userprofilefields = profile_get_custom_fields();
+        if (!empty($userprofilefields)) {
+            $userprofilefieldsarray = [];
+            // Create an array of key => value pairs for the dropdown.
+            foreach ($userprofilefields as $userprofilefield) {
+                $userprofilefieldsarray[$userprofilefield->shortname] = $userprofilefield->name;
+            }
+        }
+        $evasyssettings->add(
+            new admin_setting_configselect(
+                'booking/evasyscategoryfielduser',
+                get_string('evasyscategoryfielduser', 'mod_booking'),
+                get_string('evasyscategoryfielduser_desc', 'mod_booking'),
+                'evasysid',
+                $userprofilefieldsarray
+            )
+        );
+        try {
+            $evasys = new evasys_handler();
+            $subunitoptions = $evasys->get_subunits();
+            $periodoptions = $evasys->get_periods_for_settings();
+        } catch (SoapFault $e) {
+                $subunitoptions = [0 => get_string('evasysnotreachable', 'mod_booking')];
+        }
+        if (
+            empty($subunitoptions)
+            && empty(get_config('booking', 'evasysuser'))
+            && empty(get_config('booking', 'evasyspassword'))
+        ) {
+            $evasyssettings->add(
+                new admin_setting_description(
+                    'booking/evasyssettingswarning',
+                    '',
+                    get_string('evasyssettingswarning', 'mod_booking')
+                )
+            );
+        } else {
+            $evasyssettings->add(
+                new admin_setting_configselect(
+                    'booking/evasyssubunits',
+                    get_string('evasyssubunits', 'mod_booking'),
+                    get_string('evasyssubunits_desc', 'mod_booking'),
+                    0,
+                    $subunitoptions
+                )
+            );
+            $evasyssettings->add(
+                new admin_setting_configselect(
+                    'booking/evasysperiods',
+                    get_string('evasysperiods', 'mod_booking'),
+                    get_string('evasysperiods_desc', 'mod_booking'),
+                    0,
+                    $periodoptions
+                )
+            );
+        }
         $adminroot->add('modbookingfolder', $evasyssettings);
     }
 }
