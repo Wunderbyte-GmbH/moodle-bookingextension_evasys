966c0de23a45c79fa446880f405dc15e12c33d1b
David Ala <davidala@Mac.dhw>
2025-06-16 16:53:19 +0200

    added new placeholders

---
+++ classes/placeholders/evasys_evaluation_endtime.php
@@ -0,0 +1,79 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+namespace bookingextension_evasys\placeholders;
+
+use html_writer;
+
+defined('MOODLE_INTERNAL') || die();
+
+require_once($CFG->dirroot . '/mod/booking/lib.php');
+
+/**
+ * Resolves Placeholder for Endtime of the Evaluation.
+ *
+ * @package bookingextension_evasys
+ * @author David Ala
+ * @copyright 2025 Wunderbyte GmbH
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class evasys_evaluation_endtime {
+    /**
+     * Function which takes a text, replaces the placeholders...
+     * ... and returns the text with the correct values.
+     * @param int $cmid
+     * @param int $optionid
+     * @param int $userid
+     * @param int $installmentnr
+     * @param int $duedate
+     * @param float $price
+     * @param string $text
+     * @param array $params
+     * @param int $descriptionparam
+     * @param string $rulejson
+     * @return string
+     */
+    public static function return_value(
+        int $cmid = 0,
+        int $optionid = 0,
+        int $userid = 0,
+        int $installmentnr = 0,
+        int $duedate = 0,
+        float $price = 0,
+        string &$text = '',
+        array &$params = [],
+        int $descriptionparam = MOD_BOOKING_DESCRIPTION_WEBSITE,
+        string $rulejson = ''
+    ) {
+        global $DB;
+
+        $record = $DB->get_record('bookingextension_evasys', ['optionid' => $optionid], 'endtime');
+        if (!$record) {
+            return "";
+        }
+        return date('d.m.y H:i', $record->endtime);
+    }
+
+    /**
+     * Function determine if placeholder class should be called at all.
+     *
+     * @return bool
+     *
+     */
+    public static function is_applicable(): bool {
+        return true;
+    }
+}
