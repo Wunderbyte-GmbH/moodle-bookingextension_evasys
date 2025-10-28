ce89b67552fa3e0771f6db179aa32e029d9944a6
David Ala <davidala@Mac.dhw>
2025-10-28 11:47:37 +0100

    Version (2025102800)

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
  */
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2025102700;
+$plugin->version   = 2025102800;
 $plugin->requires  = 2024042200; // Requires this Moodle version. Current: Moodle 4.4.
 $plugin->component = 'bookingextension_evasys';
 $plugin->supported = [401, 405];
