137ec4644bd9a1169a7eb76f3cd04accd38b9576
Bernhard Fischer <bernhard.fischer@wunderbyte.at>
2026-01-05 12:57:45 +0100

    Improvement: Make sure all placeholders extend new placeholder_base class.

--- classes/placeholders/evasysevaluationstarttime.php
+++ classes/placeholders/evasysevaluationstarttime.php
@@ -18,6 +18,7 @@ namespace bookingextension_evasys\placeholders;
 
 use core\exception\moodle_exception;
 use html_writer;
+use mod_booking\placeholders\placeholder_base;
 use stdClass;
 
 defined('MOODLE_INTERNAL') || die();
@@ -32,7 +33,7 @@ require_once($CFG->dirroot . '/mod/booking/lib.php');
  * @copyright 2025 Wunderbyte GmbH
  * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-class evasysevaluationstarttime {
+class evasysevaluationstarttime extends placeholder_base {
     /**
      * Function which takes a text, replaces the placeholders...
      * ... and returns the text with the correct values.
