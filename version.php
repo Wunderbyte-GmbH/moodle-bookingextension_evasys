15aeb9250dba0785cd90e3533c7c31d64e7ce117
Magdalena Holczik <magdalena.holczik@wunderbyte.at>
2026-01-19 10:41:19 +0100

    Version (2026011900)

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
  */
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2026010801;
+$plugin->version   = 2026011900;
 $plugin->requires  = 2024042200; // Requires this Moodle version. Current: Moodle 4.4.
 $plugin->component = 'bookingextension_evasys';
 $plugin->supported = [401, 405];
